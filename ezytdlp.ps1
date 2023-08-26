b546e478d5c8caab820be88c678e07a159b3df1d
saturdayxiii <82848893+saturdayxiii@users.noreply.github.com>
2023-08-25 18:01:27 -0600

    Update ezytdlp.ps1

--- ezytdlp.ps1
+++ ezytdlp.ps1
@@ -2,7 +2,7 @@
 #It will ask for the URL, the destination directory, then give options for the filename.
 
 #just start by changing the next line to point to the location of your copy of yt-dlp.exe
-$ytdlp = "P:\scoop\apps\yt-dlp\current\yt-dlp.exe"
+$ytdlp = "C:\Users\Saturday\scoop\apps\yt-dlp\current\yt-dlp.exe"
 
 # Prompt the user to enter the URL of the YouTube playlist they want to download
 $playlistUrl = Read-Host "Enter the URL of the YouTube playlist you want to download"
@@ -13,7 +13,7 @@ $outputDirectory = Read-Host "Enter the path of the output directory for the dow
 # Prompt the user to choose the file naming format for the downloaded files
 do {
     $fileNamingFormat = Read-Host "Choose a file naming format (enter 1, 2, 3, or type in your own):
-1. %(playlist_index)02d - %(title)s.%(ext)s
+1. %(playlist_index)02d-%(title)s.%(ext)s
 2. %(upload_date)s-%(title)s[%(id)s].%(ext)s
 3. Custom format"
 } while (($fileNamingFormat -notmatch '^[123]$') -and (-not $fileNamingFormat.Trim()))
@@ -27,10 +27,10 @@ if ($fileNamingFormat -eq "3") {
 # and name the downloaded files according to the chosen file naming format
 # Add the video URL as a metadata tag labeled "url"
 if ($fileNamingFormat -eq "1") {
-    $format = "%(playlist_index)02d - %(title)s.%(ext)s"
+    $format = "%(playlist_index)02d-%(title)s.%(ext)s"
 } elseif ($fileNamingFormat -eq "2") {
     $format = "%(upload_date)s-%(title)s[%(id)s].%(ext)s"
 } else {
     $format = $customFormat
 }
-& $ytdlp $playlistUrl -f bestvideo+bestaudio --output "$outputDirectory\$format" --write-subs --add-metadata --metadata-from-title "%(url)s"
+& $ytdlp --no-overwrites --continue $playlistUrl -f bestvideo+bestaudio --output "$outputDirectory\$format" --write-subs --sub-lang en --embed-subs --add-metadata --metadata-from-title "%(url)s"
